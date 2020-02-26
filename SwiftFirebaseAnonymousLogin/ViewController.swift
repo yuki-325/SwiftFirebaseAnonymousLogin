0c162d0379fa1f2b57ef1bd08874dd941204cdfd
yuki-325 <sakeikura325@yahoo.co.jp>
2020-02-26 20:33:46 +0900

    Initial Commit

---
+++ SwiftFirebaseAnonymousLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFirebaseAnonymousLogin
+//
+//  Created by 中野勇貴 on 2020/02/26.
+//  Copyright © 2020 Nakano Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
